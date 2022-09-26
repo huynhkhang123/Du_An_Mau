27a8fc016253e6f2bcd002733bfa2a70c7caf8ae
Tham <thamtthpc03916@fpt.edu.vn>
2022-09-26 16:30:50 +0700

    Tham

--- src/com/edusys/ui/NewJFrame.java
+++ src/com/edusys/ui/NewJFrame.java
@@ -10,7 +10,7 @@ package com.edusys.ui;
  * @author Tran Thi Hong Tham
  */
 public class NewJFrame extends javax.swing.JFrame {
-
+    String tham;
     /**
      * Creates new form NewJFrame
      */
